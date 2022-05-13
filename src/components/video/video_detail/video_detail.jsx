234fc9d18fb5f23cf1b80293f9f5b1afb881c97d
wnsh3131@naver.com <wnsh3131@naver.com>
2022-05-13 19:22:05 +0900

    add iframe

--- src/components/video/video_detail/video_detail.jsx
+++ src/components/video/video_detail/video_detail.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const VideoDetail = ({video}) => (
-    <h3>{video.snippet.title}</h3>
+    <iframe id="player" type="text/html" width="640" height="360"
+  src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+  frameborder="0"></iframe>
 );
 
 export default VideoDetail;
